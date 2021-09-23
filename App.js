1faf2d4489610b27fcca40341c2a1c3fcd0cbebf
Poojan Patel <52922631+Poojan26@users.noreply.github.com>
2021-09-23 15:56:37 -0700

    Add files via upload
    Main commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HelloWorldApp() {
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+    <Text>Poojan Patel</Text>
+    <Text>Student in MAPD712</Text>
+    </View>
+  )}
